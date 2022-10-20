5e6050716050aa44b54ba2664ffee7d7180923f8
BeckysFootSlave <beckysfootslave@gmail.com>
2022-10-20 16:15:08 +0200

    Add files via upload

--- include/constants.hpp
+++ include/constants.hpp
@@ -15,13 +15,13 @@ constexpr const char APP_URL[] =            "https://github.com/Switch-Bros/Swit
 constexpr const char TAGS_INFO[] =           "https://api.github.com/repos/Switch-Bros/switchbros-updater/releases/latest";
 constexpr const char APP_FILENAME[] =        "/config/switchbros-updater/app.zip";
 
-constexpr const char NXLINKS_URL[] =         "https://raw.githubusercontent.com/rashevskyv/nx-links/master/nx-links.json";
+constexpr const char NXLINKS_URL[] =         "https://raw.githubusercontent.com/Switch-Bros/nx-links/master/nx-links.json";
 
-constexpr const char SIGPATCHES_URL[] =      "https://raw.githubusercontent.com/rashevskyv/nx-links/master/sigpatches.json";
+constexpr const char SIGPATCHES_URL[] =      "https://raw.githubusercontent.com/Switch-Bros/nx-links/master/sigpatches.json";
 constexpr const char SIGPATCHES_FILENAME[] ="/config/switchbros-updater/sigpatches.zip";
 constexpr const char HEKATE_IPL_PATH[] =    "/bootloader/hekate_ipl.ini";
 
-constexpr const char FIRMWARE_URL[] =       "https://raw.githubusercontent.com/rashevskyv/nx-links/master/firmwares.json";
+constexpr const char FIRMWARE_URL[] =       "https://raw.githubusercontent.com/Switch-Bros/nx-links/master/firmwares.json";
 constexpr const char FIRMWARE_FILENAME[] =  "/config/switchbros-updater/firmware.zip";
 constexpr const char FIRMWARE_PATH[] =      "/firmware/";
 
@@ -29,13 +29,13 @@ constexpr const char CFW_URL[] =            "https://raw.githubusercontent.com/r
 constexpr const char CFW_FILENAME[] =       "/config/switchbros-updater/atmo.zip";
 constexpr const char BOOTLOADER_FILENAME[] =       "/config/switchbros-updater/atmo.zip";
 
-constexpr const char AMS_URL[] =            "https://raw.githubusercontent.com/rashevskyv/nx-links/master/cfws.json";
+constexpr const char AMS_URL[] =            "https://raw.githubusercontent.com/Switch-Bros/nx-links/master/cfws.json";
 constexpr const char SXOS_URL[] =           "https://raw.githubusercontent.com/rashevskyv/nx-links/master/sxos.json";
 constexpr const char AMS_FILENAME[] =       "/config/switchbros-updater/atmo.zip";
 
-constexpr const char HEKATE_URL[] =         "https://raw.githubusercontent.com/rashevskyv/nx-links/master/hekate.json";
+constexpr const char HEKATE_URL[] =         "https://raw.githubusercontent.com/Switch-Bros/nx-links/master/hekate.json";
 
-constexpr const char PAYLOAD_URL[] =        "https://raw.githubusercontent.com/rashevskyv/nx-links/master/payloads.json";
+constexpr const char PAYLOAD_URL[] =        "https://raw.githubusercontent.com/Switch-Bros/nx-links/master/payloads.json";
 
 constexpr const char DEEPSEA_META_JSON[] =  "https://builder.teamneptune.net/meta.json";
 constexpr const char DEEPSEA_BUILD_URL[] =  "https://builder.teamneptune.net/build/";
@@ -65,9 +65,9 @@ constexpr const char SXOS_TITLES[] =        "/sxos/titles/";
 constexpr const char AMS_PATH[] =           "/atmosphere/";
 constexpr const char SXOS_PATH[] =          "/sxos/";
 constexpr const char REINX_PATH[] =         "/ReiNX/";
-constexpr const char CONTENTS_PATH[] =      "contents/";
-constexpr const char TITLES_PATH[] =        "titles/";
-constexpr const char SWITCHBROS_DIRECTORY_PATH[] =   "/switchbros/";
+constexpr const char CONTENTS_PATH[] =      "atmosphere/contents/";
+constexpr const char TITLES_PATH[] =        "atmosphere/contents/";
+constexpr const char SWITCHBROS_DIRECTORY_PATH[] =   "/SwitchBros_BasisPaket/";
 
 constexpr const char COLOR_PICKER_URL[] =   "https://git.io/jcpicker";
 constexpr const char JC_COLOR_URL[] =       "https://raw.githubusercontent.com/HamletDuFromage/aio-switch-updater/master/jc_profiles.json";
@@ -80,8 +80,8 @@ constexpr const char BOOTLOADER_PATH[] =    "/bootloader/";
 constexpr const char BOOTLOADER_PL_PATH[] = "/bootloader/payloads/";
 constexpr const char UPDATE_BIN_PATH[] =    "/bootloader/update.bin";
 constexpr const char REBOOT_PAYLOAD_PATH[] ="/atmosphere/reboot_payload.bin";
-constexpr const char FUSEE_SECONDARY[] =    "/atmosphere/fusee-secondary.bin";
-constexpr const char FUSEE_MTC[] =          "/atmosphere/fusee-mtc.bin";
+constexpr const char FUSEE_SECONDARY[] =    "/bootloader/fusee.bin";
+constexpr const char FUSEE_MTC[] =          "/bootloader/fusee.bin";
 
 constexpr const char AMS_ZIP_PATH[] =       "/config/switchbros-updater/ams.zip";
 constexpr const char APP_ZIP_PATH[] =       "/config/switchbros-updater/app.zip";
@@ -102,8 +102,8 @@ constexpr const char HOMEBREW[] =           "/config/switchbros-updater/language
 constexpr const char HIDDEN_AIO_FILE[] =    "/config/switchbros-updater/.switchbros-updater";
 
 constexpr const char ROMFS_PATH[] = "romfs:/";
-constexpr const char ROMFS_FORWARDER[] =    "romfs:/aiosu-forwarder.nro";
-constexpr const char FORWARDER_PATH[] =     "/config/switchbros-updater/aiosu-forwarder.nro";
+constexpr const char ROMFS_FORWARDER[] =    "romfs:/sbc-forwarder.nro";
+constexpr const char FORWARDER_PATH[] =     "/config/switchbros-updater/sbc-forwarder.nro";
 
 constexpr const char DAYBREAK_PATH[] = "/switch/daybreak/daybreak.nro";
 
